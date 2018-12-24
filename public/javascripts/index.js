7ecc140c63867a8e792ac4f2191720f241b1fdc8
maksym,plotnikov <maksym.plotnikov@lemberg.co.uk>
2018-12-24 13:07:36 +0200

    Add possibility to copy name and size

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -6,7 +6,31 @@ $('button.delete').on('click', function () {
     url: "files/delete",
     data: {"file": item},
     success: function (result) {
-      location.reload()
+      location.reload();
     }
-  })
+  });
+});
+
+function CopyToClipboard(containerid) {
+  if (document.selection) {
+    const range = document.body.createTextRange();
+    range.moveToElementText(document.getElementById(containerid));
+    range.select().createTextRange();
+    document.execCommand("copy");
+  } else if (window.getSelection) {
+    var range = document.createRange();
+    range.selectNode(document.getElementById(containerid));
+    window.getSelection().addRange(range);
+    document.execCommand("copy");
+  }
+}
+
+$('.copy-text').on('click', function () {
+  const itemId = $(this).attr("data-id");
+  CopyToClipboard(itemId);
+});
+
+$('.copy-size').on('click', function () {
+  const itemId = $(this).attr("data-size");
+  CopyToClipboard(itemId);
 });
