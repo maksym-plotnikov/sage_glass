c385117d0acd6f7a527c4b557b795fd3c341ad43
maksym,plotnikov <maksym.plotnikov@lemberg.co.uk>
2018-12-24 18:05:01 +0200

    Copy links

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,3 +1,16 @@
+
+function CopyToClipboard(containerid) {
+  console.log(document.getElementById(containerid));
+  if (window.getSelection) {
+    let range = document.createRange();
+    range.selectNode(document.getElementById(containerid));
+    window.getSelection().addRange(range);
+    document.execCommand("copy");
+    window.getSelection().removeRange(range);
+  }
+}
+
+
 $('button.delete').on('click', function () {
   const item = $(this).attr('data-item');
   $.ajax({
@@ -10,16 +23,6 @@ $('button.delete').on('click', function () {
   });
 });
 
-function CopyToClipboard(containerid) {
-  if (window.getSelection) {
-    let range = document.createRange();
-    range.selectNode(document.getElementById(containerid));
-    window.getSelection().addRange(range);
-    document.execCommand("copy");
-    window.getSelection().removeRange(range);
-  }
-}
-
 $('.copy-text').on('click', function () {
   const itemId = $(this).attr("data-id");
   CopyToClipboard(itemId);
@@ -27,5 +30,11 @@ $('.copy-text').on('click', function () {
 
 $('.copy-size').on('click', function () {
   const itemId = $(this).attr("data-size");
+  CopyToClipboard(itemId);
+});
+
+$('.copy-link').on('click', function () {
+  const itemId = $(this).prev().attr("id");
+  console.log(itemId);
   CopyToClipboard(itemId);
 });
